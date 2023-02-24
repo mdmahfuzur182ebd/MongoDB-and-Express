2c3a70c56041d39a1811f3b4606d2545262d818e
mahfuzur <mdmahfuazur7788@gamil.com>
2023-02-24 20:34:27 +0600

    Implementing router

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
   app.use(express.json());
 
-  app.use('/api/student', studentRouter);
+  app.use('/api/students', studentRouter);
 
 
 
--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-const fs = require('fs');
-
 
   const studentList = (req, res) => {
      db.getDbStudent()
