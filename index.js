4a0f200e770f87a190232127eeff04d9072244c6
mahfuzur <mdmahfuazur7788@gamil.com>
2023-02-24 10:46:33 +0600

    Handling Post Request..'

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-//Express 
-const { request, response, json } = require('express');
-const express = require('express');
-const fs = require('fs');
-const app = express();
+  const { json } = require('express');
+  const express = require('express');
+  const app = express();
+  const fs = require('fs');
+  app.use(express.json());
 
 
-app.get('/', (request, response)=>{
-    response.send(JSON.stringify(["Karim", "Rahmna"]));
-})
+  app.get('/', (request, respond) => {
+    respond.send("Hello Express js.....")
+  });
 
-app.get('/api/students', (req, res) => {
+  app.get('/api/students', (req, res) => {
     fs.readFile('./db.json', 'utf-8', (err, data) => {
-        console.log(data);
-        const student = JSON.parse(data).student
-        res.send(student);
-    } )
-})
+        // console.log(data);
+         const students = JSON.parse(data).students;
+         //console.log(student);
+         res.send(students);
+      })
+  });
+
+  app.post('/api/students', (req, res) => {
+     const student = req.body;
+     fs.readFile('./db.json', 'utf-8', (err, data) => {
+       const students = JSON.parse(data);
+       console.log(students);
+       students.students.push(student);
+       console.log(students);
+       fs.writeFile('./db.json', JSON.stringify(students), (err) =>{
+          res.send(student);
+       });
+     });
+  })
+
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log("Listening on port 3000...... ");
+    console.log(`Listening on port ${port}...... `);
 })
